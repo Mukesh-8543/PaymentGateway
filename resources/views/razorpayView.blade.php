e89857f7388d6bee1e588fa4e5013e8d3d538721
mukesh kumar yadav <my6386887@gmail.com>
2024-06-21 09:34:05 -0930

    title update

--- resources/views/razorpayView.blade.php
+++ resources/views/razorpayView.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Laravel - Razorpay Payment Gateway Integration</title>
+    <title>Mukesh - Razorpay Payment Gateway Integration</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
